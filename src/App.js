227b653fff84d0d1f51f485320a2fab846c26014
Dorl92 <dorl929@gmail.com>
2021-07-22 12:22:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import Page from './Page';
+import Homepage from './Homepage';
+import { Route, Switch } from 'react-router-dom';
+import SnackbarProvider from 'react-simple-snackbar'
+
+import ProjectsList from './ProjectsList';
+import ProjectInfo from './ProjectInfo';
+import ContactMe from './ContactMe';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition key={location.key} timeout={500} classNames="Page">
+            <Switch location={location}>
+              <Route exact path="/" render={(routeProps) => <Page><Homepage {...routeProps} /></Page>} />
+              <Route exact path="/portfolio" render={(routeProps) => <Page><ProjectsList {...routeProps} /></Page>} />
+              <Route exact path="/portfolio/:projectName" render={(routeProps) => <Page><ProjectInfo {...routeProps} projectName={routeProps.match.params.projectName} /></Page>} />
+              <Route exact path="/contact-me" render={(routeProps) => <Page><SnackbarProvider><ContactMe {...routeProps} /></SnackbarProvider></Page>} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
     </div>
   );
 }

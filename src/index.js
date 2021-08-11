b445b88ada6db7b5d7d48dcffc2fede1fa1a52c6
Dorl92 <dorl929@gmail.com>
2021-08-11 14:41:07 +0300

    remove basename url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter basename="https://dorlahiyani-portfolio.netlify.app/">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
